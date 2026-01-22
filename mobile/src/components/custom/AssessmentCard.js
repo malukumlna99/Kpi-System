c80b52b07b5e36fece2482ae31fb097a5640eb5a
Septa Maulana <malukumlna99@gmail.com>
2026-01-22 13:33:41 +0700

    Update AssessmentCard.js

--- mobile/src/components/custom/AssessmentCard.js
+++ mobile/src/components/custom/AssessmentCard.js
@@ -1,4 +1,3 @@
-// ==================== mobile/src/components/custom/AssessmentCard.js ====================
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { format } from 'date-fns';
@@ -133,3 +132,4 @@ const styles = StyleSheet.create({
 });
 
 export default AssessmentCard;
+
