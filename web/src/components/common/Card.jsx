f618bbab5a46c2b48389856de131b47e2ce8aeb8
Septa Maulana <malukumlna99@gmail.com>
2026-01-22 13:16:28 +0700

    Update Card.jsx

---
+++ web/src/components/common/Card.jsx
@@ -0,0 +1,20 @@
+// ==================== web/src/components/common/Card.jsx ====================
+import React from 'react';
+import { cn } from '../../utils/helpers';
+
+const Card = ({ children, className, hoverable = false, ...props }) => {
+  return (
+    <div
+      className={cn(
+        'bg-white rounded-lg shadow-sm border border-gray-200 p-6',
+        hoverable && 'card-hover cursor-pointer',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
